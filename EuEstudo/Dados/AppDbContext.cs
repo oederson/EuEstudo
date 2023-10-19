b6a8ba4b19f783833c6584b47432170a1f0e073f
oederson <eder.hu@live.com.pt>
2023-10-18 23:50:18 -0300

    iniciado proejto

---
+++ EuEstudo/Dados/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EuEstudo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EuEstudo.Dados
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<PerguntasModel> Perguntas { get; set; }
+    }
+}
---
+++ EuEstudo/Models/PerguntasModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EuEstudo.Models
+{
+    public class PerguntasModel
+    {
+        public int Id { get; set; }
+        public string Questao { get; set; }
+        public string Resposta { get; set; }
+    }
+}
