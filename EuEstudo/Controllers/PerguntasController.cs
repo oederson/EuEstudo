044e858c13d30b7babb981ab32bb026992487cb4
oederson <eder.hu@live.com.pt>
2023-10-19 18:42:12 -0300

    Adicionado cadastrar pergunta

---
+++ EuEstudo/Controllers/PerguntasController.cs
@@ -0,0 +1,44 @@
+﻿using EuEstudo.Dados;
+using EuEstudo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EuEstudo.Controllers
+{
+    public class PerguntasController : Controller
+    {
+        private readonly AppDbContext _database;
+        public PerguntasController(AppDbContext database)
+        {
+            _database = database;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<PerguntasModel> questoes = _database.Perguntas;
+            return View(questoes);
+        }
+        [HttpGet]
+        public IActionResult Cadastrar() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        
+        public IActionResult Cadastrar( PerguntasModel pergunta)
+        {
+
+            Console.WriteLine($"Questao{pergunta.Questao}");
+
+            if (ModelState.IsValid) 
+            {
+            Console.WriteLine("Entrei no if");
+            _database.Perguntas.Add(pergunta);
+            _database.SaveChanges();
+
+            return RedirectToAction("Index");
+                }
+            return View(pergunta);
+
+        }
+    }
+}
--- EuEstudo/Models/PerguntasModel.cs
+++ EuEstudo/Models/PerguntasModel.cs
@@ -1,9 +1,13 @@
-﻿namespace EuEstudo.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EuEstudo.Models
 {
     public class PerguntasModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Necessario informar a questão")]
         public string Questao { get; set; }
+        [Required(ErrorMessage = "Necessario informar a resposta")]
         public string Resposta { get; set; }
     }
 }
